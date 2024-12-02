438debc0df9a5082b0e75f8c073eb79160562b63
Pankaj8839 <pankajrajput8839@gmail.com.>
2024-12-02 16:26:04 +0530

    add last change

--- src/App.js
+++ src/App.js
@@ -39,9 +39,11 @@ function App() {
     const fetchData = async () => {
       try {
         const response = await fetch('https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json');
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
         const data = await response.json();
         setCompleteData(data);
-   
        
       } catch (error) {
         alert(error);
